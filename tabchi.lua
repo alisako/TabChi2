cbd8c729150650fe607b06598f30c7505bfc8bdf
Morteza_DF <morteza9del044@gmail.com>
2017-02-09 14:28:25 +0330

    Update tabchi.lua

--- tabchi.lua
+++ tabchi.lua
@@ -128,7 +128,7 @@ function process(msg)
     if msg.text:match("^[!/#]setanswer") and is_sudo(msg) and #matches == 3 then
       redis:hset("tabchi:" .. tabchi_id .. ":answers", matches[2], matches[3])
       redis:sadd("tabchi:" .. tabchi_id .. ":answerslist", matches[2])
-      return "cracked by @M3YS4M - Answer for " .. matches[2] .. " set to " .. matches[3]
+      return "cracked by @morteza_df - Answer for " .. matches[2] .. " set to " .. matches[3]
     end
   end
   do
@@ -217,7 +217,7 @@ function process(msg)
           }, dl_cb, nil)
         else
           local text = [[
-*Tabchi Moded by @M3YS4M -- Normal stats :*
+*Tabchi Moded by @morteza_df -- Normal stats :*
 Users : ]] .. pvs .. [[
 
 Groups : ]] .. gps .. [[
@@ -271,7 +271,7 @@ Saved links : ]] .. links
     if msg.text:match("^[!/#]addedmsg") and is_sudo(msg) and #matches == 2 then
       if matches[2] == "on" then
         redis:set("tabchi:" .. tabchi_id .. ":addedmsg", true)
-        return "Added Message Turned On *cracked by @M3YS4M*"
+        return "Added Message Turned On *cracked by @morteza_df*"
       elseif matches[2] == "off" then
         redis:del("tabchi:" .. tabchi_id .. ":addedmsg")
         return "Added Message Turned Off"
@@ -285,7 +285,7 @@ Saved links : ]] .. links
     if msg.text:match("^[!/#]markread") and is_sudo(msg) and #matches == 2 then
       if matches[2] == "on" then
         redis:set("tabchi:" .. tabchi_id .. ":markread", true)
-        return "Markread Turned On *open by @M3YS4M*"
+        return "Markread Turned On *open by @morteza_df*"
       elseif matches[2] == "off" then
         redis:del("tabchi:" .. tabchi_id .. ":markread")
         return "Markread Turned Off"
@@ -476,9 +476,9 @@ function process_stats(msg)
   end
 end
 function process_links(text_)
-  if text_:match("https://telegram.me/joinchat/%S+") then
+  if text_:match("https://t.me/joinchat/%S+") then
     local matches = {
-      text_:match("(https://telegram.me/joinchat/%S+)")
+      text_:match("(https://t.me/joinchat/%S+)")
     }
     tdcli_function({
       ID = "CheckChatInviteLink",
